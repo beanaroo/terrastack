bf4e575e1d667a247b51480381cc900c7f9e87b9
frankotron <frank@frankotron.se>
2018-08-10 10:22:26 +0200

    Add markdown readme instead of rst

--- README.rst
+++
@@ -1,6 +0,0 @@
-Terrastack
-========================
-
-This simple project provides a way to define terrastack specifications through the Python language.
-
----------------
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
@@ -14,6 +14,7 @@ setup(
     version='0.1.0',
     description='Create terraform specifications with code',
     long_description=readme,
+    long_description_content_type='text/markdown',
     author='Frank Hammar',
     author_email='frank@frankotron.se',
     url='https://github.com/frankotron/terrastack',
